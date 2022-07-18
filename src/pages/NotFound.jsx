21d491e00ca67a2287a27ac9f70832fa28a98fb4
Phyo Pyae Sone Han <68275444+phyohan18@users.noreply.github.com>
2022-07-18 14:42:46 +0630

    Testing UI

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -75,7 +75,12 @@ export default function NotFound() {
                         </li>
                         <li className="ml-1">
                             <div title="Change Language" className="dropdown dropdown-end">
-                                <div tabIndex="0" className="btn gap-1 normal-case btn-ghost">Lang</div>
+                                <div tabIndex="0" className="btn gap-1 normal-case btn-ghost">
+                                {i18next.language == 'en' ? <img className="w-6 h-6 rounded-lg" src={"https://flagicons.lipis.dev/flags/4x3/gb.svg"} alt="en"/> :
+                                    <img className="w-6 h-6 rounded-lg" src={"https://flagicons.lipis.dev/flags/4x3/mm.svg"} alt="mm"/>}
+                                    <span className="uppercase text-base hidden md:block">{i18next.language}</span> 
+                                    <RiArrowDropDownLine size={27} />
+                                </div>
                                 <div className="dropdown-content bg-base-200 text-base-content rounded-t-box rounded-b-box top-px mt-16 w-56 overflow-y-auto shadow-2xl">
                                     <ul className="menu menu-compact gap-1 p-3" tabindex="0">
                                         <li>
