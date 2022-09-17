d29ea887488c20a1838f173bdd1af1371ddcf3d7
Phyo Pyae Sone Han <68275444+phyohan18@users.noreply.github.com>
2022-09-17 15:33:53 +0630

    Fixed Pagination Design

--- src/components/layout/Pagination.jsx
+++ src/components/layout/Pagination.jsx
@@ -5,9 +5,9 @@ export default function Pagination({ currentPage,paginate,pageNumbers }){
             { pageNumbers.map( (page,index) => {
                 return (
                     page === '...' ?
-                        <button className="font-semibold rounded-md mx-1 mb-1" key={index}>{page}</button>
+                        <button className="w-4 xs:w-auto font-semibold rounded-md mx-1 mb-1" key={index}>{page}</button>
                         :<button 
-                            className={`tab font-semibold rounded-md mx-1 ${currentPage === page ? 'bg-teal-500 shadow-2xl text-accent-content border border-transparent' : 'btn-ghost' }`} 
+                            className={`w-4 xs:w-auto tab font-semibold rounded-md mx-1 ${currentPage === page ? 'bg-teal-500 shadow-2xl text-accent-content border border-transparent' : 'btn-ghost' }`} 
                             style={ { transition: "all 0.2s ease" }}
                             onClick={ () => paginate(page) }
                             key={index}
