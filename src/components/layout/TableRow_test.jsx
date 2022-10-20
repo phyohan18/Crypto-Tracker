79365b892c59922ca2602578d79542b4e51d4157
Phyo Pyae Sone Han <68275444+phyohan18@users.noreply.github.com>
2022-10-20 18:03:07 +0630

    Fixing Home Table

--- src/components/layout/TableRow_test.jsx
+++ src/components/layout/TableRow_test.jsx
@@ -1,5 +1,5 @@
 import React,{ Suspense , lazy } from 'react'
-import {formatBalanceCommas} from '../../hooks/globalFun'
+import { formatBalanceCommas } from '../../hooks/globalFun'
 const Sparkline = lazy(() => import('./Sparkline'))
 export function TableRow({coinInfo, currency}) {
     return (
@@ -22,13 +22,13 @@ export function TableRow({coinInfo, currency}) {
             </td> */}
             <td className="text-right xl:text-center dark:border-y-0">
                 <div className="md:flex">
-                    <div className="w-full font-medium md:font-normal"><span className="uppercase">hi&nbsp;</span>hi</div>
+                    <div className="w-full font-medium md:font-normal"><span className="uppercase">hi&nbsp;</span>{formatBalanceCommas(coinInfo.current_price)}</div>
                     <div className={`block font-semibold md:font-light md:hidden ${coinInfo.price_change_percentage_24h > 0 ? 'text-emerald-600' : 'text-red-500'}`}>{coinInfo.price_change_percentage_24h}%</div>
                 </div>
             </td>
             <td className="hidden md:table-cell dark:border-y-0"><div className={`font-semibold ${coinInfo.price_change_percentage_24h > 0 ? 'text-emerald-600' : 'text-red-600'}`}>{coinInfo.price_change_percentage_24h}%</div></td>
             {/* <td className="hidden xs:table-cell dark:border-y-0 "><span className="uppercase">{currency}&nbsp;</span>&nbsp;{formatBalanceCommas(coinInfo.market_cap)}</td> */}
-            <td className="hidden xs:table-cell dark:border-y-0 "><span className="uppercase">hi&nbsp;</span>&nbsp;hi</td>
+            <td className="hidden xs:table-cell dark:border-y-0 "><span className="uppercase">hi&nbsp;</span>&nbsp;{formatBalanceCommas(coinInfo.market_cap)}</td>
             <td className="hidden lg:flex items-center justify-center dark:border-y-0">
                 <div className="w-40 h-12">
                     <Suspense fallback={<div className="bg-slate-200 dark:bg-slate-600 rounded mx-auto w-40 h-12"></div>}>
