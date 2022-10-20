2a3b1ed62caa9eff60881d8786bac22aa6110d83
Phyo Pyae Sone Han <68275444+phyohan18@users.noreply.github.com>
2022-10-20 18:28:26 +0630

    Fixing Home Table

--- src/components/layout/TableRow_test.jsx
+++ src/components/layout/TableRow_test.jsx
@@ -1,5 +1,5 @@
 import React,{ Suspense , lazy } from 'react'
-import { formatBalanceCommas } from '../../hooks/globalFun'
+
 const Sparkline = lazy(() => import('./Sparkline'))
 export function TableRow({coinInfo, currency}) {
     const formatBalanceCommas = (x) =>  x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
@@ -23,7 +23,7 @@ export function TableRow({coinInfo, currency}) {
             </td> */}
             <td className="text-right xl:text-center dark:border-y-0">
                 <div className="md:flex">
-                    <div className="w-full font-medium md:font-normal"><span className="uppercase">{currency}&nbsp;</span>{formatBalanceCommas(19212.37)}</div>
+                    <div className="w-full font-medium md:font-normal"><span className="uppercase">{currency}&nbsp;</span>{formatBalanceCommas(coinInfo.current_price)}</div>
                     <div className={`block font-semibold md:font-light md:hidden ${coinInfo.price_change_percentage_24h > 0 ? 'text-emerald-600' : 'text-red-500'}`}>{coinInfo.price_change_percentage_24h}%</div>
                 </div>
             </td>
